79aea6a5289de918986ad6a092338a9b47c39755
GITHIKA DASI <GITHIKADASI@GMAIL.com>
2025-11-11 18:17:25 +0000

    conditional statement

---
+++ conditional statement.py
@@ -0,0 +1,9 @@
+num= 15
+if num > 10:
+    print(num, "is a positive number")
+
+num1 =-5
+if num1 > 0:
+    print(num1,"is a positive number")
+
+
--- countingthenotes.py
+++ countingthenotes.py
@@ -1,7 +1,10 @@
-£ Taking total amount as input from User
+# Taking total amount as input from User
 Amount =int(input("Please Enter Amount for Withdraw :"))
-£ Calculating the number of notes of differentrr demonominations
+# Calculating the number of notes of differentrr demonominations
 note_1 = Amount//100
 note_2 = (Amount%100)//50
 note_3 = ((Amount%100)%50)//10
-print("")
+print("notes of 100 rupees" , note_1)
+print("notes of 50 rupee" , note_2)
+print("notes of 10 rupee" , note_3)
+

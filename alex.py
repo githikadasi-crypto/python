6870245a2ec2492327763798a4ce24b1ede3fbf4
GITHIKA DASI <GITHIKADASI@GMAIL.com>
2025-12-09 18:15:03 +0000

    hello

---
+++ alex.py
@@ -0,0 +1,6 @@
+name= "Alex"
+age=0
+if name == "alex" or name == "John" and age >= 2:
+ print ( "hello welcome")
+else :
+ print ("goodbye")
---
+++ mean value.py
@@ -0,0 +1,13 @@
+mean1 =38
+wrongnumber=36
+correctnumberf=56
+totalnumber=40
+#sum of 40 numbers
+sum =mean1*totalnumber
+print("the sum of 40 number: " ,sum)
+#correct sum of these numbers
+num2=sum-((wrongnumber)-(correctnumberf))
+print("sum-((wrongnumber)-(correctnumberf)): ",num2)
+#the correct mesn
+mean2=num2/totalnumber
+print(mean2)
